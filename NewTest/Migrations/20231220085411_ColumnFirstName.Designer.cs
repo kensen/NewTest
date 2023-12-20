ec8974fc8c1659e25892ba44b4fc671971f2a281
yuqingyuan <kensen@foxmail.com>
2023-12-20 17:04:02 +0800

    EF 迁移，修改 Student 实体

--- NewTest/Migrations/20231220085411_ColumnFirstName.Designer.cs
+++ NewTest/Migrations/20231220085411_ColumnFirstName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NewTest.Data;
 
@@ -11,9 +12,11 @@ using NewTest.Data;
 namespace NewTest.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20231220085411_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
