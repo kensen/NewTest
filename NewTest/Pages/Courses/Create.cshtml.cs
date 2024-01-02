3be1fbd87b49bc4bed5ad9065134c2a1d0a25d4f
yuqingyuan <kensen@foxmail.com>
2024-01-02 14:43:10 +0800

    添加课程页面

---
+++ NewTest/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NewTest.Data;
+using NewTest.Modles;
+
+namespace NewTest.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly NewTest.Data.SchoolContext _context;
+
+        public CreateModel(NewTest.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ NewTest/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NewTest.Data;
+using NewTest.Modles;
+
+namespace NewTest.Pages.Courses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly NewTest.Data.SchoolContext _context;
+
+        public DetailsModel(NewTest.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Course = course;
+            }
+            return Page();
+        }
+    }
+}
