eea49c80cd69261c6e0f7c4f2ce53cc338f3da7e
yuqingyuan <kensen@foxmail.com>
2024-01-23 14:55:20 +0800

    up

--- NewTest/Modles/SchoolViewModels/InstructorIndexData.cs
+++ NewTest/Modles/SchoolViewModels/InstructorIndexData.cs
@@ -3,8 +3,10 @@
     public class InstructorIndexData
     {
         public IEnumerable<Instructor> Instructors { get; set; }
+
         public IEnumerable<Course> Courses { get; set; } 
 
         public IEnumerable<Enrollment> Enrollments { get; set; }
+
     }
 }
