9e3428e763967cc1a3470401d32ef50fac6c5b54
yuqingyuan <kensen@foxmail.com>
2024-01-24 14:36:33 +0800

    新增 Instructors 页面

---
+++ NewTest/Pages/Instructors/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NewTest.Data;
+using NewTest.Modles;
+
+namespace NewTest.Pages.Instructors
+{
+    public class CreateModel : PageModel
+    {
+        private readonly NewTest.Data.SchoolContext _context;
+
+        public CreateModel(NewTest.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Instructor Instructor { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Instructors.Add(Instructor);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ NewTest/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NewTest.Data;
+using NewTest.Modles;
+
+namespace NewTest.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly NewTest.Data.SchoolContext _context;
+
+        public IndexModel(NewTest.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Instructor> Instructor { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Instructor = await _context.Instructors.ToListAsync();
+        }
+    }
+}
